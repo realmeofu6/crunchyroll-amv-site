ff6bd9f884802621d8446a5d3e3d55ebe44134e4
realmeofu6 <realm.eofu.sa@gmail.com>
2025-03-23 22:12:18 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,3 @@
 export default function handler(req, res) {
-  // Simulate user session check
-  res.status(200).json({ user: "crunchyroll-user" });
+  res.redirect("https://www.crunchyroll.com/login");
 }
